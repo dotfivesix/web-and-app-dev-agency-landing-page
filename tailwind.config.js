01325a844bac57259f9557f1dfcc0a88ccee11a8
DarkDev <mda.business.on@gmail.com>
2023-01-28 18:54:18 +0500

    Completed Hero and Design Methodology

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./index.html","./index.css","./script.js"],
   theme: {
     extend: {
       fontFamily: {
         "poppins": "'Poppins', sans-serif"
       },
       colors: {
+        "very-dark": "#06070F",
         "dark": "#10111A",
-        "dark-secondary": "#171826",
+        "dark-secondary": "#25262E",
         "pink": "#CA64BF",
         "orange": "#F37A53",
         "green": "#48BB9B",
